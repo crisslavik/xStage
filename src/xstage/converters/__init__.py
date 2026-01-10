53978120a12991740bf24045970582c2a2d8e370
Cristian Slavik <slavik@studio.local>
2026-01-09 22:29:07 -0800

    Fix ConverterDialog import error - make it optional
    - Remove ConverterDialog from converters/__init__.py (it's in ui.editors)
    - Make ConverterDialog import optional in main __init__.py
    - Add conditional inclusion in __all__ list

--- src/xstage/converters/__init__.py
+++ src/xstage/converters/__init__.py
@@ -7,7 +7,6 @@ from .material_creator import MaterialCreator, MaterialShaderType
 from .material_validator import MaterialValidator, MaterialIssue
 
 try:
-    from .converter_ui import ConverterDialog
     from .adobe_converter import AdobeUSDConverter
     __all__ = [
         "USDConverter",
@@ -17,27 +16,14 @@ try:
         "MaterialValidator",
         "MaterialIssue",
         "AdobeUSDConverter",
-        "ConverterDialog",
     ]
 except ImportError:
-    try:
-        from .adobe_converter import AdobeUSDConverter
-        __all__ = [
-            "USDConverter",
-            "ConversionOptions",
-            "MaterialCreator",
-            "MaterialShaderType",
-            "MaterialValidator",
-            "MaterialIssue",
-            "AdobeUSDConverter",
-        ]
-    except ImportError:
-        __all__ = [
-            "USDConverter",
-            "ConversionOptions",
-            "MaterialCreator",
-            "MaterialShaderType",
-            "MaterialValidator",
-            "MaterialIssue",
-        ]
+    __all__ = [
+        "USDConverter",
+        "ConversionOptions",
+        "MaterialCreator",
+        "MaterialShaderType",
+        "MaterialValidator",
+        "MaterialIssue",
+    ]
 
